65cb4486b4800f9512722601c94201dc4c72ee09
Your Name <you@example.com>
2024-10-25 11:54:44 +0200

    aggiornamento icone

--- src/it/unibs/pajc/BarraInfo.java
+++ src/it/unibs/pajc/BarraInfo.java
@@ -8,9 +8,18 @@ public class BarraInfo extends JPanel {
     private JLabel title;
     private JLabel recordLabel;
     private int record;
+    private ImageIcon mela;
+    private ImageIcon coppa;
 
     public BarraInfo() {
         setLayout(new BorderLayout());
+        ImageIcon originalMela = new ImageIcon(getClass().getResource("/images/school.png"));
+        ImageIcon originalCoppa = new ImageIcon(getClass().getResource("/images/trophy.png"));
+
+        Image ridemensioneMela = originalMela.getImage().getScaledInstance(20,20, Image.SCALE_SMOOTH);
+        mela = new ImageIcon(ridemensioneMela);
+        Image ridimensioneCoppa = originalCoppa.getImage().getScaledInstance(20,20, Image.SCALE_SMOOTH);
+        coppa = new ImageIcon(ridimensioneCoppa);
 
         title = new JLabel("SNAKE");
         title.setHorizontalAlignment(SwingConstants.CENTER);
@@ -18,12 +27,15 @@ public class BarraInfo extends JPanel {
         title.setFont(new Font("Tahoma", Font.BOLD, 25));
         add(title, BorderLayout.NORTH);
 
-        labelMeleMangiate = new JLabel("Mele Mangiate: 0");
+        labelMeleMangiate = new JLabel("0");
+        labelMeleMangiate.setIcon(mela);
         labelMeleMangiate.setFont(new Font("Arial", Font.BOLD, 18));
         labelMeleMangiate.setHorizontalAlignment(SwingConstants.CENTER);
+        labelMeleMangiate.setVerticalAlignment(SwingConstants.BOTTOM);
         add(labelMeleMangiate, BorderLayout.WEST);
 
-        recordLabel = new JLabel("Record: 0");
+        recordLabel = new JLabel(" 0");
+        recordLabel.setIcon(coppa);
         recordLabel.setFont(new Font("Arial", Font.BOLD, 18));
         recordLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(recordLabel, BorderLayout.EAST);
@@ -32,10 +44,12 @@ public class BarraInfo extends JPanel {
     }
 
     public void aggiornaMeleMangiate(int meleMangiate) {
-        labelMeleMangiate.setText("Mele Mangiate: " + meleMangiate);
+        labelMeleMangiate.setIcon(mela);
+        labelMeleMangiate.setText(" " + meleMangiate);
         if (meleMangiate > record) {
             record = meleMangiate;
-            recordLabel.setText("Record: " + record);
+            recordLabel.setIcon(coppa);
+            recordLabel.setText(" " + record);
         }
 
     }
